92183eab2ceacb62ff3ec625349b278688ac0280
David Francos <opensource@davidfrancos.net>
2023-11-06 23:45:21 +0100

    fix: fixed date

--- caearagon/footer.php
+++ caearagon/footer.php
@@ -18,7 +18,7 @@ namespace WP_Rig\WP_Rig;
     <div class=footer-content>
 
         <div class="footer-data">
-            <p> 8 de junio de 2023<br/> 9:00 a 18:30 h</p>
+            <p> 6 de junio de 2024</p>
             <p> <a href="https://www.fundacionibercaja.es/nuestros-espacios/campus-ibercaja" target="_blank">Monasterio Cogullada. Campus Ibercaja</a><br/> Carretera de Cogullada, 127, Zaragoza</p>
             <p> <a href="mailto:congreso.aragon.agilidad@gmail.com">congreso.aragon.agilidad@gmail.com</a></p>
         </div>

22f94cbb98082cadbd587000534204a3e5fd3f54
Roberto Sanz <rsciriano@gmail.com>
2024-11-10 18:21:31 +0000

    Add Google Analytics

--- caearagon/header.php
+++ caearagon/header.php
@@ -25,6 +25,15 @@ namespace WP_Rig\WP_Rig;
 		<?php
 	}
 	?>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q4MQ0ELVV9"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-Q4MQ0ELVV9');
+	</script>	
 
 	<?php wp_head(); ?>
 </head>
